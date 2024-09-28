70fc4f305577889f2ac182a2dbc95890a9b59cc1
kpthanh01 <kphamthanh01@gmail.com>
2024-09-28 00:30:45 -0400

    added logic in the userGuess(). Where if the user's input matches a name from the character list array, then remove that name from the array

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,10 @@ const inputTag = document.getElementById('search')
 const imageTag = document.getElementById('characterImg')
 let leagueChampions = []
 
+let renderInfo = async () => {
+  await getLeagueChampions()
+}
+
 let getLeagueChampions = async () => {
   let response = await axios.get(riotAPI)
     .then(res => { return res.data.data })
@@ -13,20 +17,23 @@ let getLeagueChampions = async () => {
     elementTag.id = `${item.toLowerCase()}`
     elementTag.innerHTML = `<img class="champion" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${item}_0.jpg" alt="champion">`
     imageTag.appendChild(elementTag)
-    leagueChampions.push(item)
+    leagueChampions.push(item.toLowerCase())
   })
 }
 
-let renderInfo = async () => {
-  await getLeagueChampions()
-  console.log(leagueChampions)
-}
-
-let userInput = (event) => {
-  
+let userGuess = (event) => {
+  let guess = event.target.value
+  let remainingAnswers = leagueChampions
+  if (guess.length > 1) {
+    remainingAnswers.forEach((item, index) => {
+      if(item === guess) {
+        remainingAnswers.splice(index, 1)
+      }
+    })
+  }
 }
 
 renderInfo()
 
-inputTag.addEventListener('input', userInput)
+inputTag.addEventListener('input', userGuess)
 
